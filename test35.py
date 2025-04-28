ecf39b99fda085717c6e8a619177bc8345d8b7ba
cestbryan_ng <ngoupeyoubryan9@gmail.com>
2025-04-28 02:29:36 +0100

    update test35

--- test35.py
+++ test35.py
@@ -5,10 +5,3 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
 import numpy as np
 import tensorflow as tf
 import keras
-
-# tenseurs
-a = np.array([[1, 2], [3, 4]])
-a = tf.constant(a)
-print((a + 2).numpy())
-
-
